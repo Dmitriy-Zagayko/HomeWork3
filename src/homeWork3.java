644bd1ee721d2f34382e81b4167f5d4c2a2b1166
Dmitriy-Zagayko <70841267+Dmitriy-Zagayko@users.noreply.github.com>
2021-02-12 00:38:02 +0200

    Initial commit

---
+++ src/homeWork3.java
@@ -0,0 +1,19 @@
+public class homeWork3 {
+
+    public void task1 (double angle, double speed) {
+        double gravity = 9.8;
+        double distance = ((speed * speed) / gravity) * Math.sin(Math.toRadians(2 * angle));
+    }
+
+    public void task2 (double v1, double v2, double s, double time) {
+        double t = Math.abs(v1 + v2) * time + s;
+    }
+
+    public void task3 () {
+
+    }
+
+    public void task4 () {
+
+    }
+}
